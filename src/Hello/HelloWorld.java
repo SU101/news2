db4c0e0d9b3d4a85f475c1bf7a4c30d693b2bbe3
lenovo <lenovo@192.168.70.1>
2018-09-12 13:25:17 +0800

    修改了HelloWorld.java

--- src/Hello/HelloWorld.java
+++ src/Hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("version1.0:Hello World!");
 		System.out.println("version1.1:change this file");
-		//System.out.println("version1.2:first change");
+		System.out.println("version1.2:first change");
 		//System.out.println("version1.3:second change");
 	}
 }
